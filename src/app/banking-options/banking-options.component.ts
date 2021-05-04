925b446c089cc5434bf4f3db4c856f0bf4532c89
rohitdandwani <rohitdandwani97@gmail.com>
2021-05-05 01:24:32 +0400

    Animations introduced for banking options

--- src/app/banking-options/banking-options.component.ts
+++ src/app/banking-options/banking-options.component.ts
@@ -1,9 +1,28 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-banking-options',
   templateUrl: './banking-options.component.html',
-  styleUrls: ['./banking-options.component.scss']
+  styleUrls: ['./banking-options.component.scss'],
+  animations: [
+    trigger('slideIn', [
+      state('void', style({
+        opacity: 0
+      })),
+      transition('void => *', [
+        style({
+          transform: 'translateX(-20px)'
+        }),
+        animate(700)
+      ]),
+
+      transition('* => void', [
+        animate(700)
+      ])
+    ])
+  ]
 })
 export class BankingOptionsComponent implements OnInit {
 
